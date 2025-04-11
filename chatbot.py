9e8da7ceeed27e79ed20e38e221fbb38b1fd74a7
Sai Sekhar <sai.sekhar0909@gmail.com>
2025-04-11 15:31:01 -0500

    practice2

---
+++ chatbot.py
@@ -0,0 +1,17 @@
+# Simple Chatbot
+
+name = input("Hi! What's your name? ")
+print("Nice to meet you,", name)
+
+while True:
+    message = input("You: ").lower()  # Get user input and make it lowercase
+
+    if message == "hello":
+        print("Bot: Hello! How can I help you?")
+    elif message == "how are you?":
+        print("Bot: I'm just code, but I'm doing great!")
+    elif message == "bye":
+        print("Bot: Goodbye! Have a great day!")
+        break
+    else:
+        print("Bot: Sorry, I didn't understand that.")
